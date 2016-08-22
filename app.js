72bf66d9bc8505474eaa32878b72009b6cc5cad5
Vikas Bhoria <vikas@Vikass-MacBook-Pro-2.local>
2016-08-22 11:48:58 +0530

    Deleted redundant html and js files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, function () {
