476ec3859e5f41c21bd9589592cbde1cbdbdb8af
vckybhr <vbhoria7@gmail.com>
2016-08-24 20:05:20 +0530

    .eslintrc and eslintrc.js file added

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "node": true
+    },
+    "extends": ["eslint:recommended", "angular"],
+    "rules": {
+        "no-console":0,
+        "angular/log":0
+    }
+};
