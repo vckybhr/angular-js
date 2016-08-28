77ccb53823643b635cc2a3343c501ff6172f9104
vckybhr <vbhoria7@gmail.com>
2016-08-28 23:42:49 +0530

    config folder added

--- src/con.js
+++ src/con.js
@@ -1,37 +1,37 @@
 var app = angular.module('app',['ngRoute','ngAnimate','ngTouch']);
 
-app.config(function($routeProvider){
-	$routeProvider.
-		when('/',{
-			templateUrl: 'html/home.html',
-			controller:  'homeCtrl'
+// app.config(function($routeProvider){
+// 	$routeProvider.
+// 		when('/',{
+// 			templateUrl: 'html/home.html',
+// 			controller:  'homeCtrl'
 
-			}).
-		when('/competition',{
-			templateUrl: 'html/competition.html',
-			controller:  'competitionCtrl'
-			}).
-		when('/fixtures',{
-			templateUrl: 'html/fixture.html',
-			controller:  'fixtureCtrl'
-			}).
-		// when('/teams',{
-		// 	templateUrl: 'html/teams.html',
-		// 	controller:  'teamCtrl'
-		// 	}).
-		when('/teams/:key/:teamkey/players',{
-			templateUrl: 'html/teamPlayers.html',
-			controller:  'teamPlayerCtrl'
-			}).
-		when('/:id/Competeams',{
-			templateUrl: 'html/competitionTeam.html',
-			controller:  'competitionTeamCtrl'
-			}).
-		otherwise({
-			redirectTo:'/'
-		});
+// 			}).
+// 		when('/competition',{
+// 			templateUrl: 'html/competition.html',
+// 			controller:  'competitionCtrl'
+// 			}).
+// 		when('/fixtures',{
+// 			templateUrl: 'html/fixture.html',
+// 			controller:  'fixtureCtrl'
+// 			}).
+// 		// when('/teams',{
+// 		// 	templateUrl: 'html/teams.html',
+// 		// 	controller:  'teamCtrl'
+// 		// 	}).
+// 		when('/teams/:key/:teamkey/players',{
+// 			templateUrl: 'html/teamPlayers.html',
+// 			controller:  'teamPlayerCtrl'
+// 			}).
+// 		when('/:id/Competeams',{
+// 			templateUrl: 'html/competitionTeam.html',
+// 			controller:  'competitionTeamCtrl'
+// 			}).
+// 		otherwise({
+// 			redirectTo:'/'
+// 		});
 
-}); 
+// }); 
 
 app.controller('homeCtrl', function($scope){
         $scope.slides = [
